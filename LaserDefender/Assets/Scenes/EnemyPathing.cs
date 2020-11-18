b9a006843f7865f3eaddc25c4c4a2d76ed6b0b53
sherdan659 <sherdan659@gmail.com>
2020-11-18 15:40:02 +0100

    Made the Enemy move
    Linked the wavepoints to enemie

--- LaserDefender/Assets/Scenes/EnemyPathing.cs
+++ LaserDefender/Assets/Scenes/EnemyPathing.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
+    [SerializeField] List<Transform> wayPoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    int wayPointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = wayPoints[wayPointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        if(wayPointIndex < wayPoints.Count)
+            //if(wayPointIndex <= wayPoints.Count - 1)
+        {
+            var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
+            var targetPosition = wayPoints[wayPointIndex].position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+                wayPointIndex++;
         
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ LaserDefender/Assets/Scenes/WaveConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+
+    [SerializeField] GameObject eneryPrefab;
+
+    [SerializeField] GameObject pathPrefab;
+
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+
+    [SerializeField] float spawnRandomFactor = 0.3f;
+
+    [SerializeField] int numberOfEnemies = 5;
+
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab()
+    {
+        return eneryPrefab;
+    }
+
+}
